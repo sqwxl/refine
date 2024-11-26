53fb42fd314de0cad836969c63d0f1fd1729730f
Nicolas Epstein <55964909+sqwxl@users.noreply.github.com>
2024-11-26 14:34:19 -0500

    Unswap message and description fields

--- packages/antd/src/providers/notificationProvider/index.tsx
+++ packages/antd/src/providers/notificationProvider/index.tsx
@@ -39,8 +39,8 @@ export const notificationProvider: NotificationProvider = {
     } else {
       staticNotification.open({
         key,
-        description: message,
-        message: description ?? null,
+        description: description ?? null,
+        message,
         type,
       });
     }
@@ -85,8 +85,8 @@ export const useNotificationProvider = (): NotificationProvider => {
       } else {
         notification.open({
           key,
-          description: message,
-          message: description ?? null,
+          description: description ?? null,
+          message,
           type,
         });
       }
